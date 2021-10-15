ef81f245f11cb784d1533f0019aa044ecd864828
Nick <nickvichai@gmail.com>
2021-10-15 07:20:09 -0500

    modified to deploy objects into json file

--- indeedScraper/app.py
+++ indeedScraper/app.py
@@ -1,8 +1,6 @@
 import requests
 import json
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from bs4 import BeautifulSoup
 
 def extract(page):
@@ -21,24 +19,26 @@ def extract(page):
 def getJobInfo(soup):
 
     allJobs = []
+    id = 0
     
     a = soup.find_all('a', class_ = ('tapItem', 'fs-unmask'))
 
     for jobs in a:
         title = jobs.find('span', title = True)
-        company = jobs.find('span', class_ = ('companyName')).text
-        location = jobs.find('div', class_ = ('companyLocation')).text
-        description = jobs.find('div', class_ = ('job-snippet')).text
+        company = jobs.find('span', class_ = ('companyName')).text.strip()
+        location = jobs.find('div', class_ = ('companyLocation')).text.strip()
+        description = jobs.find('div', class_ = ('job-snippet')).text.strip()
         salary = jobs.find('span', class_ = ('salary-snippet'))
-        date = jobs.find('span', class_ = ('date')).text
+        date = jobs.find('span', class_ = ('date')).text.strip()
 
         if (salary == None):
             hasSalary = None
         else:
             hasSalary = salary.text
-        
+        id = id + 1
         myJson = {
-            'Title':title['title'],
+            'Id':id,
+            'Title':title['title'].strip(),
             'Company':company,
             'Location':location,
             'Description':description,
@@ -50,41 +50,40 @@ def getJobInfo(soup):
         
     return allJobs
 
-app = Flask(__name__)
-
-""" STILL RESEARCHING HOW THIS WORKS
-
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-
-class Job(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    company = db.Column(db.String(80), nullable=True)
-    location = db.Column(db.String(80), nullable=True)
-    description = db.Column(db.String(240), nullable=True)
-    salary = db.Column(db.String(80), nullable=True)
-    datePosted = db.Column(db.String(80), nullable=True)
-
-    def __repr__(self):
-        return f"{self.title} - {self.company} - {self.location} - {self.description} - {self.salary} - {self.datePosted}" 
-
-"""
-
-@app.route('/')
-def index():
-    return 'Hello!'
-
-@app.route('/jobs')
 def getJobs():
-    jobs = []
-    for x in range(4):
+    for x in range(1):
         c = extract(x + 1)
         jobInfo = getJobInfo(c)
-        toJson = json.dumps(jobInfo, indent=4, separators=(", ", ": "))
-        jobs.append(toJson)
-    return {'jobs': jobs}
+    return jobInfo
+
+def writeJson(data, filename="./web/resources/indeedJobs.json"):
+    with open (filename, "w") as f:
+        f.write(json.dumps(data, indent=4))
+
+def clearJson(filename="./web/resources/indeedJobs.json"):
+    with open("./web/resources/indeedJobs.json", "r") as json_file:
+        data = json.load(json_file)
+        n = 0
+        if (data == []):
+            pass
+        else:
+            for i in range(len(data)):
+                i = 0
+                if (data[i]["Id"] == (n + 1)):
+                    data.pop(i)
+                    n = n + 1
+        writeJson(data)
+        json_file.close()
+        
+clearJson() ## To clear objects so we can update
+
+with open("./web/resources/indeedJobs.json", "r") as json_file: ## Read in data and append to list
+    data = json.load(json_file)
+    y = getJobs()
+    for job in y:
+        data.append(job)
+    json_file.close()
+
+writeJson(data) ## Dump list into json file
 
 
