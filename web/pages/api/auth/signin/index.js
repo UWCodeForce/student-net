99f138ad9bb94963ef753595a9b8fb51878fc616
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2021-10-17 13:13:50 +0530

    cookie response & db session storage set up, bug fix
    fixed bug where an invalid email login lookup returns status code 500 instead of a 401 (email not found in database)
    
    implemented logic through which a successful login returns a cookie with a randomized id, and should insert it into the database provided it has the correct table and schema.
    
    We don't actually do anything with the sessions yet, still have to implement use cases for that.

--- web/pages/api/auth/signin/index.js
+++ web/pages/api/auth/signin/index.js
@@ -1,36 +1,62 @@
 import nextConnect from 'next-connect'
 import bcrypt from 'bcrypt'
-import { query } from '../../../../utils/query'
+import { query, pool } from '../../../../utils/query'
+import session from 'express-session'
+import MySQLStore from 'express-mysql-session'
+
+const store = MySQLStore(session) 
+const sessionStore = new store({
+  expiration: 3600, 
+  createDatabaseTable: false, // probably should create it manually
+  schema: {
+    tableName: 'sessions_test',
+    columnNames: {
+        session_id: 'session_id',
+        expires: 'session_expires',
+        data: 'session_data'
+    }
+  }
+}, pool)
 
 const handler = nextConnect()
+
+    .use(session({
+            name: "sid",
+            secret: process.env.SESSION_SECRET,
+            resave: false, // no need to store the session again everytime for every user
+            saveUninitialized: false,
+            cookie: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 3600 // 1 hr, might want to add to serverconstants.js later
+            },
+            store: sessionStore
+        }))
 
     .post(async (req, res) => {
         const { email, password } = req.body 
 
-        if (!email || !password) res.status(400).json({ error: 'Email or Password cannot be blank' })
+        if (!email || !password) return res.status(400).json({ error: 'Email or Password cannot be blank' })
 
         try {
             const results = await query(`
               SELECT password FROM users WHERE email = ?
             `, [email])
 
+            if (results[0].length<1) return res.status(401).json({ error: 'Email not found!' })
+
             // will need to find a better way to do this in the future
             // currently cannot directly parse the 'RowPacket' that mysql2 returns by using results[0].password
             const hashedPW = JSON.parse(JSON.stringify(results[0][0].password))
-
-            if (!results[0]) return res.status(401).json({ error: 'Email not found!' })
-
+            
             const valid = await bcrypt.compare(password, hashedPW)            
 
-            if (valid) {  
-              /*
-                todo: 
-                  - create a session for the user, store it in db
-                  - store session in cookie/localstorage/sessionstorage
-                  - check session with server upon refresh 
-                  - two factor authentication ?
-              */
-              return res.status(200).json({ message: 'Success!' })
+            if (valid) {
+              // because req.session.saveUninitialized = false, the session won't send a cookie & store the data until
+              // we make changes to it. which only happens inside this validation check 
+              req.session.userId = email
+
+              return res.status(200).json({ message: 'Success!', session: req.session })
             }
 
             else if (!valid) return res.status(401).json({ error: 'Incorrect Password!' })
