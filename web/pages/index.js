a11a640cde2a5f3169f87093ac1c2e9134df12a9
Nick <nickvichai@gmail.com>
2021-10-25 01:43:17 -0500

    created homepage in JSX

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,27 +1,30 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
     <div>
+      <Head>
         <meta charSet="utf-8" />
         <meta name="description" content />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Homepage</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/railscasts.min.css" />
-        <link rel="stylesheet" href="../styles/globals.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlinkLight.js/9.12.0/styles/railscasts.min.css" />
+      </Head>
         <nav>
-          <div className="logo link--light"><strong>Student-Net</strong></div>
-          <ul className="menu">
-            <div className="menu__item toggle"><span /></div>
-            <li className="menu__item"><a href="#" className="link link--light">Home</a></li>
-            <li className="menu__item"><a href="#" className="link link--light">Forum</a></li>
-            <li className="menu__item"><a href="#" className="link link--light">Courses</a></li>
-            <li className="menu__item"><a href="#" className="link link--light">Housing</a></li>
-            <li className="menu__item"><a href="#" className="link link--light">Jobs</a></li>
-            <li className="menu__item"><a href="#" className="link link--light">RateMyProf</a></li>
+          <div className={`${styles.logo} ${styles.link} ${styles.linkLight}`}><strong>Student-Net</strong></div>
+          <ul className={styles.menu}>
+            <div className={`${styles.menu__item} ${styles.toggle}`}><span /></div>
+            <li className={styles.menu__item}><Link href="#"><a className={`${styles.link}`}>Home</a></Link></li>
+            <li className={styles.menu__item}><Link href="#"><a className={`${styles.link}`}>Forum</a></Link></li>
+            <li className={styles.menu__item}><Link href="#"><a className={`${styles.link}`}>Courses</a></Link></li>
+            <li className={styles.menu__item}><Link href="#"><a className={`${styles.link}`}>Housing</a></Link></li>
+            <li className={styles.menu__item}><Link href="#"><a className={`${styles.link}`}>Jobs</a></Link></li>
+            <li className={styles.menu__item}><Link href="#"><a className={`${styles.link}`}>RateMyProf</a></Link></li>
           </ul>
         </nav>
       </div>
