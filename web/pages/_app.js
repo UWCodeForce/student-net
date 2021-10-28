b4d2a35a796707bfcb8b531b3b044d94c6285752
Nick <nickvichai@gmail.com>
2021-10-27 22:29:35 -0500

    Changed to track Chakra UI

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import '../styles/utilities.css'
+import { ChakraProvider } from "../node_modules/@chakra-ui/react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
