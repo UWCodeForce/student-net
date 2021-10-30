0edea0ce1c6884ec95c5df41573b2dbdea6f2e90
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2021-10-31 01:34:52 +0530

    Chakra UI additions signin/signup

--- web/components/auth/SignIn.js
+++ web/components/auth/SignIn.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { validateSignIn } from '../../utils/formvalidation'
-import styles from '../../styles/auth.module.css'
 import { 
     Input,
-    Box,
+    InputGroup,
+    InputRightElement,
     Flex,
+    VStack,
     Button,
     Heading,
-    Divider
+    FormControl,
+    FormLabel,
+    Alert,
+    AlertIcon,
+    AlertDescription,
+    Link,
+    Box
 } from "@chakra-ui/react"
 
 export default function SignIn() {
@@ -34,11 +41,15 @@ export default function SignIn() {
 
         let res = await fetch('./api/auth/signin', {
             method: 'POST',
+            credentials: 'include',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(body),
         })
 
-        if (res.status===200) Router.reload()
+        if (res.status===200) {
+            res = await res.json()
+            setResponse(res)
+        }
 
         else { 
             res = await res.json()
@@ -47,38 +58,62 @@ export default function SignIn() {
     }
 
     return (
-        <Flex height="100vh" alignItems="center" justifyContent="Center" backgroundColor="gray.700">
-
-            <form onSubmit={e => onSubmit(e)}>
-                <Flex direction="column" background="gray.100" p={12} rounded={6}>
+        <Flex height="100vh" align="center" justify="center" backgroundColor="gray.700">
+                <VStack className="noselect" background="gray.100" p="3rem" align="center" justify="center" spacing="1rem">
                     <Heading>Sign In</Heading>
 
                     {response && response.message && 
-                    <Box className={styles.alert} style={{backgroundColor: "green"}}>
-                        <p>{response.message}</p>
-                    </Box>}
+                    <Alert status="success" rounded="1.5rem" p="0.75rem 1rem" justify="center" align="center" width="fit-content">
+                        <AlertIcon/>
+                        <AlertDescription>{response.message}</AlertDescription>
+                    </Alert>}
 
                     {response && response.error && 
-                    <Box >
-                        <p>{response.error}</p>
-                    </Box>}
+                    <Alert status="error" rounded="1rem" justify="center" align="center" width="fit-content">
+                        <AlertIcon/>
+                        <AlertDescription>{response.error}</AlertDescription>
+                    </Alert>}
+                    
+                    <form onSubmit={e => onSubmit(e)}>
+                        <VStack justify="center" align="center" spacing="0.5rem">
+                            <FormControl isRequired>
+                                <FormLabel>Email</FormLabel>
+                                <Input name="email"/>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel>Password</FormLabel>
+                                <PasswordInput name="password" />
+                            </FormControl>
+                            <Box>
+                                <Button mt="1rem" colorScheme="red" type="submit">Sign In</Button>
+                            </Box>
+                        </VStack>
+                    </form>
 
-                    <span>&nbsp;</span>
-                    <Box>
-                        <Input /* type="email" -> html5 email validation, probably not very secure */ 
-                        name="email" placeholder="Email"/>
-                    </Box>
-                    <Box className={styles.field}>
-                        <Input type="password" name="password" placeholder="Password"/>
-                    </Box>
-                    <Button type="submit">Sign In</Button>
-                    <span>&nbsp;</span>
-                    <Box className={styles.links}>
-                    <Box><a onClick={() => Router.push('/signup')}>Sign Up</a></Box>
-                    <Box><a>Forgot Password</a></Box>
-                    </Box>
-                </Flex>
-            </form>
+                    <Flex justify="space-between" width="100%" direction="row"> 
+                        <p><Link style={{textDecoration: 'inherit'}} onClick={() => Router.push('/signup')}>Sign Up</Link></p>
+                        <p><Link style={{textDecoration: 'inherit'}}>Forgot Password</Link></p>
+                    </Flex>
+                </VStack>
         </Flex>
     )
+}
+
+function PasswordInput({ name }) {
+  const [show, setShow] = React.useState(false)
+  const handleClick = () => setShow(!show)
+
+  return (
+    <InputGroup>
+      <Input
+        type={show ? "text" : "password"}
+        name={name}
+      />
+      <InputRightElement width="4.5rem">
+        <Button h="75%" size="sm" onClick={handleClick}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  )
 }
