523caf751b9c5dfa1e40efba22677280ff831b75
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2021-11-11 06:14:55 +0530

    fix query

--- web/pages/api/auth/signup/index.js
+++ web/pages/api/auth/signup/index.js
@@ -1,34 +1,23 @@
-import nextConnect from 'next-connect'
-import bcrypt from 'bcrypt'
-import { query } from '../../../../utils/query'
-import { SALT_ROUNDS } from '../../../../utils/systemconstants'
+import nextConnect from "next-connect";
+import bcrypt from "bcrypt";
+import { query } from "../../../../utils/query";
+import { SALT_ROUNDS } from "../../../../utils/systemconstants";
 
-const handler = nextConnect()
+const handler = nextConnect().post(async (req, res) => {
+  const { email, password } = await req.body;
+  if (!email || !password)
+    res.json({ error: "Email or Password cannot be blank!" });
 
-    .post(async (req, res) => {
-        const { email, password } = await req.body 
-        
-        if (!email || !password) res.json({ error: 'Email or Password cannot be blank!' })
+  try {
+    const hashedPW = await bcrypt.hash(password, SALT_ROUNDS);
+    await query(`INSERT INTO users(email, password) VALUES(?, ?)`, [
+      email,
+      hashedPW,
+    ]);
+    return res.status(200).json({ message: "Registered!" });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
 
-        try {
-
-          /*
-            todo:
-              - validate password: see if it has 8 digits and 1 symbol 
-              - add check to see if email has @uwinnipeg.ca or @webmail.uwinnipeg.ca
-              - add check to see if email exists
-              - send validation link to email
-          */
-
-            const hashedPW = await bcrypt.hash(password, SALT_ROUNDS)
-            const results = await query(`
-              INSERT INTO users(email, password) VALUES(?, ?)   
-            `, [email, hashedPW])
-        
-            return res.status(200).json({ message: 'Registered!' })
-          } catch (e) {
-            res.status(500).json({ message: e.message })
-          }
-    })
-
-export default handler
+export default handler;
