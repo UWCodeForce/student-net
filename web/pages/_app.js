3486698e7ef06c768dd64ad868c217fe7de05312
Steven <steven.simko1000@gmail.com>
2021-12-27 17:56:16 -0600

    Added temp content to homepage. Added footer

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import '../styles/utilities.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider>
 			<Component {...pageProps} />
+			<Footer />
 		</ChakraProvider>
 	);
 }
