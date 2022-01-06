2aed7597e575f3c55e19370363e34bc38a0c7003
Nick <nickvichai@gmail.com>
2022-01-06 16:54:09 -0600

    added jobs page

---
+++ web/pages/jobs.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Heading } from '@chakra-ui/react';
+import Posts from '../components/Posts';
+import Pagination from '../components/Pagination';
+
+const App = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(5);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            const res = await axios.get('http://localhost:3000/api/jobs');
+            setPosts(res.data);
+            setLoading(false);
+        }
+
+        fetchPosts();
+    }, []);
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    return (
+        <Box textAlign="center">
+            <br />
+            <Heading fontSize='48' margin='10px'>
+                Available Jobs
+            </Heading>
+            <Posts posts={currentPosts} loading={loading} />
+            <br />
+            <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
+            <br />
+        </Box>
+    );
+};
+
+export default App;
