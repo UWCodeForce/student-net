12ba4529e60a9f9490bc1b4653083fad114a3624
Nick <nickvichai@gmail.com>
2022-01-06 16:53:07 -0600

    added pagination

---
+++ web/components/Pagination.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    Grid,
+    GridItem,
+    Link,
+    Center
+} from '@chakra-ui/react'
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <Center>
+            <Grid templateColumns='repeat(6, 1fr)' gap={40} backgroundColor='blue.100' width='100%' border='1px' >
+                {pageNumbers.map(number => (
+                    <Link onClick={() => paginate(number)} _hover={{ backgroundColor: "gray.500" }} padding='25px'>
+                        <GridItem key={number}>
+                            {number}
+                        </GridItem>
+                    </Link>
+                ))}
+            </Grid>
+        </Center>
+    )
+}
+
+export default Pagination
