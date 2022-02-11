7c5961b565614ece4c9ab3722f5ef96ed7dd9df6
luis <luislayawen@gmail.com>
2022-02-11 14:25:15 -0600

    First commit

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -3,7 +3,7 @@ import { Box, Text } from '@chakra-ui/react';
 
 export default function Footer() {
 	return (
-		<Box w="100%" bg="blue" align="center">
+		<Box w="100%" bg="blue" align="center" >
 			<Text pt="1%" pb="1%" color="white">
 				&copy; Student-Net. All rights reserved.
 			</Text>
